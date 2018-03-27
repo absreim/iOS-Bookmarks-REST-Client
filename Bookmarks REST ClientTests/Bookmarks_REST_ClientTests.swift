3a0e88b156726a40d13c8260baa73d23b3e9d432
Brook Li <emcdynamicist@gmail.com>
2018-03-27 16:02:59 -0400

    Initial Commit

---
+++ Bookmarks REST ClientTests/Bookmarks_REST_ClientTests.swift
@@ -0,0 +1,36 @@
+//
+//  Bookmarks_REST_ClientTests.swift
+//  Bookmarks REST ClientTests
+//
+//  Created by Brook Li on 3/27/18.
+//  Copyright © 2018 Brook Li. All rights reserved.
+//
+
+import XCTest
+@testable import Bookmarks_REST_Client
+
+class Bookmarks_REST_ClientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
